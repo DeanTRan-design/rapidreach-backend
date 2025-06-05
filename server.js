af3ab1a4a6538444289926401ceffa1e59ef73ab
DeanTRan-design <deantran.art@gmail.com>
2025-06-05 21:09:59 +0800

    update server file

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const port = process.env.PORT || 3000
 
 // database connection----------------------------------------
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,    
+    useNewUrlParser: true,      
 })
     .then(() =>{
         console.log("db connected!")
