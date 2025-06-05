0ad817f5c4625833468e57283f1cf72292a4cc00
DeanTRan-design <deantran.art@gmail.com>
2025-06-05 21:16:05 +0800

    update server file

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const path = require("path");
 const port = process.env.PORT || 3000
 
 // database connection----------------------------------------
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,      
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() =>{
         console.log("db connected!")
     })
